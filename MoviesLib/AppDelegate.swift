5708b5cd00834f797524e8e6c018711e6d106023
Eric Brito <eric_brito@msn.com>
2020-09-22 23:11:55 -0300

    Printando "apagar depois"

--- MoviesLib/AppDelegate.swift
+++ MoviesLib/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window?.tintColor = UIColor(named: "Main")
+        print("apagar depois")
         
         return true
     }
